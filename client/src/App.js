bf6ae2e1f9c445484b3a1585a29dfec3da5461da
sichan <sichan1301@naver.com>
2023-12-05 18:50:57 +0900

    톡방 텍스트, 시간 출력

--- client/src/App.js
+++ client/src/App.js
@@ -3,26 +3,31 @@ import './App.css';
 import RoomListPage from './components/roomListPage';
 import ChattingRoom from './components/chattingRoom';
 import { Routes, Route} from 'react-router-dom'
+import { useState } from 'react';
 
 export const roomList = [
   {
     id:1,
     roomName:'1번 톡방',
-    members:[]
+    members:[],
+    text:[]
   },
   {
     id:2,
     roomName:'2번 톡방',
-    members:[]
+    members:[],
+    text:[]
   },
   {
     id:3,
     roomName:'3번 톡방',
-    members:[]
+    members:[],
+    text:[]
   }
 ]
 
 function App() {
+
   return (
     <Routes>
       <Route exact path="/room" element={<RoomListPage rooms={roomList} />} />
--- client/src/components/chattingRoom.js
+++ client/src/components/chattingRoom.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useMemo } from "react";
+import { useState, useEffect, useMemo, useCallback } from "react";
 import { useParams } from "react-router-dom"
 import styled from 'styled-components'
 import io from 'socket.io-client'
@@ -9,40 +9,60 @@ const ChattingRoom = ({rooms}) => {
   const params = useParams()
   const room = useMemo(()=>rooms.find(item => String(item.id) === params.id),[params])
   const [text,setText] = useState('')
-  const [receivedMessage,setReceivedMessage] = useState('')
+  const [textList,setTextList] = useState([])
   
-  
-  const sendMessage = async (e,roomNum) => {
+  const checkIsUserMe = useMemo(() => {
+    if(room.id === socket.id){
+      return true
+    }else{
+      return false
+    }
+  },[socket,room]) 
+
+  const sendMessage = useCallback(async(e,roomNum) => {
     e.preventDefault();
     if(text!==""){
       const messageInfo = {
         roomNum:roomNum,
         text,
         name:socket.id,
-        time:new Date(Date.now).getHours() + ":" + new Date(Date.now).getMinutes()
+        time:new Date().getHours() + ":" + new Date().getMinutes()
       }
       await socket.emit('send_message',messageInfo)
     }
-  }
+    setText('')
+  },[socket,text])
   
   useEffect(()=>{
     socket.emit('join_room',room.id)
-  },[socket])
+  },[])
 
   useEffect(()=>{
     socket.on('receive_message',(messageData)=>{
-      setReceivedMessage(messageData.text)
+      setTextList((prev)=>[...prev,messageData])
     })
   },[socket])
 
   return(
     <ChattingRoomContainer>
       <h3>{room.roomName}</h3>
-      <p>{receivedMessage}</p>
+    
+      {
+        checkIsUserMe 
+        ?
+          <div>
+            {textList.map((e,idx)=> <MyTextArea key ={String(idx)}> <MyText>{e.text}</MyText> <Time>{e.time}</Time></MyTextArea>)}
+          </div>
+        :
+          <div>
+            {textList.map((e,idx)=> <YourTextArea key ={String(idx)}> <YourText>{e.text}</YourText> <Time>{e.time}</Time> </YourTextArea>)}
+          </div>
+      }
+
       <InputArea>
         <PlusBtn>+</PlusBtn>
         <Form onSubmit={(e)=>{sendMessage(e,room.id)}}>
-          <Input onChange = {(e)=>{setText(e.target.value)}} />
+          <Input onChange = {(e)=>{setText(e.target.value)}} value = {text} />
           <Button>전송</Button>
         </Form>
       </InputArea>
@@ -60,6 +80,39 @@ const ChattingRoomContainer = styled.div`
   border: none;
   position:relative;
   padding:0;
+  overflow:hidden;
+`
+
+const MyTextArea = styled.div`
+  display: flex;
+  align-items: end;
+  justify-content: flex-end;
+  margin-right: 15px;
+`
+
+const YourTextArea = styled(MyTextArea)`
+  justify-content: flex-start;
+  margin-left: 15px;
+`
+
+const MyText = styled.p`
+  background-color: #f7e600;
+  border-radius: 8px;
+  padding: 8px;
+  max-width: 200px;
+  font-size: 12px;
+  margin-bottom:5px;
+  font-weight: 500;
+`
+
+const YourText = styled(MyText)`
+  background-color: white;
+`
+
+const Time = styled.span`
+  font-size:11px;
+  margin-left:6px;
+  margin-bottom:5px;
 `
 
 const InputArea = styled.div`
